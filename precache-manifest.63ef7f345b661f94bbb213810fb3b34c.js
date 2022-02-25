30d05e9de23c000e4d2308e823f7c61a64b92b7d
defi-jennie <defijennie@gmail.com>
2022-02-25 09:10:33 -0500

    Updates

---
+++ precache-manifest.63ef7f345b661f94bbb213810fb3b34c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4eb733e04ee13f8e4e6f3bca73234c2",
+    "url": "/volhalla-web-app/index.html"
+  },
+  {
+    "revision": "e1ebea89780bac9ac59a",
+    "url": "/volhalla-web-app/static/css/main.e7d786df.chunk.css"
+  },
+  {
+    "revision": "22791fb3e1eaa2e626a6",
+    "url": "/volhalla-web-app/static/js/2.1b57e6a4.chunk.js"
+  },
+  {
+    "revision": "89f81c9a7155409dff498da02d0f8e32",
+    "url": "/volhalla-web-app/static/js/2.1b57e6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ebea89780bac9ac59a",
+    "url": "/volhalla-web-app/static/js/main.dca635a8.chunk.js"
+  },
+  {
+    "revision": "a783b8d188f6b713df0f",
+    "url": "/volhalla-web-app/static/js/runtime-main.3472be7d.js"
+  }
+]);
